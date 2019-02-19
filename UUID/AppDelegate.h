234b1c80a379e133adbbcd5acd50bc2b16328bd5
chenxiaosong <smallhorse87@163.com>
2019-02-19 11:28:56 +0800

    Initial Commit

---
+++ UUID/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UUID
+//
+//  Created by chenxiaosong on 2019/2/19.
+//  Copyright © 2019年 chenxiaosong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UUID/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UUID
+//
+//  Created by chenxiaosong on 2019/2/19.
+//  Copyright © 2019年 chenxiaosong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
